18797c63d7ad45d1e8333dc271acee2e39cb35af
CoollGod <89750194+CoollGod@users.noreply.github.com>
2022-03-25 03:09:14 +0300

    delete type of holiday

--- main.py
+++ main.py
@@ -24,15 +24,15 @@ name = []
 description = []
 country = []
 date = []
-type_of_holiday = []
 location = []
+
+"""Наполнение листов данными с API"""
 
 for holiday in data['response']['holidays']:
     name.append(holiday['name'])
     description.append(holiday['description'])
     country.append(holiday['country']['name'])
     date.append(holiday['date']['iso'])
-    type_of_holiday.append(holiday['type'])
     location.append(holiday['locations'])
 
 holiday_dict = {
@@ -40,13 +40,12 @@ holiday_dict = {
     'holiday_description': description,
     'country_of_holiday': country,
     'date_of_holiday': date,
-    'type_of_holiday': type_of_holiday,
     'locations_of_holiday': location
 }
 
-holiday_df = pd.DataFrame(holiday_dict, columns=['name', 'holiday_description', 'country_of_holiday', 'date_of_holiday', 'type_of_holiday', 'locations_of_holiday'])
+holiday_df = pd.DataFrame(holiday_dict, columns=['name', 'holiday_description', 'country_of_holiday', 'date_of_holiday', 'locations_of_holiday'])
 
-
+holiday_df.to_csv('new_csv.csv', encoding='utf-8', index=False)
 
 print(holiday_df)
-print(date, sep='\n')
+# print(date, sep='\n')
